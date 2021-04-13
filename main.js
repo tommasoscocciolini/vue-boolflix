fca3a8558957af4eee470958870568e5e649a1d5
tommaso <scocciolinitommaso@gmail.com>
2021-04-13 16:54:05 +0200

    aggiunta bandiera e ricerca serie tv

--- main.js
+++ main.js
@@ -2,17 +2,26 @@ var app= new Vue({
   el: '#root',
   data:{
     nameSearch: '',
-    filmResult: [],
+    queryResult: [],
+    api_key: 'e99307154c6dfb0b4750f6603256716d',
+    uri: 'https://api.themoviedb.org/3',
+    lang: 'it',
   },
   methods: {
     searchFilm: function (input) {
       if (input != '') {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query='+ input)
+        axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${input}&language=${this.lang}`)
             .then((response) => {
-              console.log(response.data.results);
-              var searchRes = response.data.results;
-              this.filmResult = [...searchRes];
+              //console.log(response.data.results);
+              var searchResFilm = response.data.results;
+              this.queryResult = [...searchResFilm];
+              axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${input}&language=${this.lang}`)
+                  .then((response) => {
+                    //console.log(response.data.results);
+                    this.queryResult = [...this.queryResult, ...response.data.results];
+                  });
             });
+
       }
 
     },
@@ -21,6 +30,10 @@ var app= new Vue({
       console.log(posterLink);
       return 'https://image.tmdb.org/t/p/w200'+posterLink;
     },
+    getFlag: function(lang) {
+      var flagLink = `https://www.unknown.nu/flags/images/${lang}-100`;
+      return flagLink;
+    }
   },
   created: function () {
 
