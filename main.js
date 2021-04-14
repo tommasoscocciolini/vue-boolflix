9b5a879cd5eefc9b7defbfd1e36f26e439ca8975
tommaso <scocciolinitommaso@gmail.com>
2021-04-14 14:15:42 +0200

    aggiornato layout e aggiunta di shortcuts

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var app= new Vue({
     api_key: 'e99307154c6dfb0b4750f6603256716d',
     uri: 'https://api.themoviedb.org/3',
     lang: 'it',
+    starFull: '<i class="fas fa-star"></i>',
+    emptyStar: '<i class="far fa-star"></i>',
+    titlePage: 'Popular Movies',
   },
   methods: {
     searchFilm: function (input) {
@@ -21,21 +24,91 @@ var app= new Vue({
                     this.queryResult = [...this.queryResult, ...response.data.results];
                   });
             });
-
+            this.titlePage = 'Search Results'
       }
 
     },
+
     getPoster: function(film) {
       var posterLink = film.poster_path;
-      console.log(posterLink);
+      //console.log(posterLink);
       return 'https://image.tmdb.org/t/p/w200'+posterLink;
     },
+
     getFlag: function(lang) {
       var flagLink = `https://www.unknown.nu/flags/images/${lang}-100`;
       return flagLink;
-    }
-  },
-  created: function () {
+    },
 
+    rateToStarsFull: function(rating) {
+      var starNr = rating/2;
+      starNr = Math.round(starNr);
+      var star5Array = [];
+      for (var i = 0; i < starNr; i++) {
+        star5Array.push(this.starFull);
+      }
+
+      return star5Array;
+    },
+
+    rateToStarsEmpty: function(rating) {
+      var starNr = rating/2;
+      starNr = Math.round(starNr);
+      var star5Array = [];
+      for (var i = 0; i < 5-starNr; i++) {
+        star5Array.push(this.emptyStar);
+      }
+
+      return star5Array;
+    },
+
+    popularMovies: function() {
+      axios.get(`${this.uri}/movie/popular?api_key=${this.api_key}`)
+          .then((response) => {
+            //console.log(response.data.results);
+            //console.log(response.data);
+            this.queryResult = [ ...response.data.results];
+          });
+          this.titlePage = 'Popular Movies'
+    },
+
+    topRatedMovies: function() {
+      axios.get(`${this.uri}/movie/top_rated?api_key=${this.api_key}&language=it`)
+          .then((response) => {
+            //console.log(response.data.results);
+            //console.log(response.data);
+            this.queryResult = [ ...response.data.results];
+          });
+          this.titlePage = 'Top Rated Movies'
+    },
+
+    popularTV: function() {
+      axios.get(`${this.uri}/tv/popular?api_key=${this.api_key}&language=it`)
+          .then((response) => {
+            //console.log(response.data.results);
+            //console.log(response.data);
+            this.queryResult = [ ...response.data.results];
+          });
+          this.titlePage = 'Popular Tv series'
+    },
+
+    topRatedTV: function() {
+      axios.get(`${this.uri}/tv/top_rated?api_key=${this.api_key}&language=it`)
+          .then((response) => {
+            //console.log(response.data.results);
+            //console.log(response.data);
+            this.queryResult = [ ...response.data.results];
+          });
+          this.titlePage = 'Top Rated Tv series'
+    },
+
+  },
+  created() {
+    axios.get(`${this.uri}/movie/popular?api_key=${this.api_key}`)
+        .then((response) => {
+          //console.log(response.data.results);
+          //console.log(response.data);
+          this.queryResult = [...this.queryResult, ...response.data.results];
+        });
   }
 });
